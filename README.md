3dcaa113349dbcfe7a09cce5006ec21da797d55b
SlKzᵍᵐ <105301169+slkzgm@users.noreply.github.com>
2023-07-08 14:49:17 +0200

    More elegant script, implement stealth plugin and extra to allow Google sign in

---
+++ README.md
@@ -0,0 +1,28 @@
+# GeoGame Automation
+
+## Description
+This is a Node.js application that uses Puppeteer to automate location identification and data fetching during GeoGame sessions. The application streamlines the gameplay and enhances understanding of Google Maps API interactions.
+
+**⚠️ Disclaimer: Use this script at your own risk. Using automation scripts can violate the terms of service of the website, potentially resulting in penalties. Please respect the rules of the site. This script is shared for educational purposes only. It is recommended to look into the potential improvements section if you wish to use this script in a more discrete manner.**
+
+## Setup and Usage
+1. Clone this repository and install the dependencies by running `npm install` in the project directory.
+2. Run the script by using the command `node index.js`. This will open a new browser instance.
+3. Once the GeoGame page is open, log in with your account.
+4. Start a new game.
+5. Once the game has started, refresh the page. The script will automatically validate the round for you.
+6. After validation, you can proceed to the next round. Repeat step 5 for each new round.
+
+The current script will validate each round immediately after the page refresh. Make sure you refresh the page only when you're ready to validate the round.
+
+## Potential Improvements
+The following features could be implemented for further enhancement:
+- **Random delay**: Introduce a random delay (with user-defined maximum value) before validation to make the usage less conspicuous and more human-like.
+- **Automatic refresh**: Automate the page refresh after each validation. This would allow the user to leave the script running without any further interaction once the game has started.
+- **Randomized location**: Introduce a slight random variation to the latitude and longitude values that are submitted, to avoid pinpoint accuracy every time and mimic more natural gameplay.
+
+## Contributing
+Feel free to fork the project, open issues or pull requests to suggest improvements or add new features.
+
+## License
+This project is licensed under the MIT License. See the LICENSE file for more details.
--- index.js
+++ index.js
@@ -1,78 +1,78 @@
-const puppeteer = require('puppeteer')
+const puppeteer = require('puppeteer-extra')
+
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin())
+
+const sendAnswer = async (page, gameToken, lat, lng) => {
+    return page.evaluate(async (gameToken, lat, lng) => {
+        const response = await fetch(`https://www.geoguessr.com/api/v3/games/${gameToken}`, {
+            "headers": {
+                "accept": "*/*",
+                "accept-language": "en,fr;q=0.9",
+                "content-type": "application/json",
+                "sec-ch-ua": "\"Not.A/Brand\";v=\"8\", \"Chromium\";v=\"114\", \"Google Chrome\";v=\"114\"",
+                "sec-ch-ua-mobile": "?0",
+                "sec-ch-ua-platform": "\"Windows\"",
+                "sec-fetch-dest": "empty",
+                "sec-fetch-mode": "cors",
+                "sec-fetch-site": "same-origin",
+                "x-client": "web"
+            },
+            "referrer": `https://www.geoguessr.com/game/${gameToken}`,
+            "referrerPolicy": "strict-origin-when-cross-origin",
+            "body": JSON.stringify({
+                token: gameToken,
+                lat: lat,
+                lng: lng,
+                timeout: false
+            }),
+            "method": "POST",
+            "mode": "cors",
+            "credentials": "include"
+        });
+        return await response.json();
+    }, gameToken, lat, lng);
+}
 
 async function run() {
-    const browser = await puppeteer.launch({
-        headless: false
-    });
-    const page = await browser.newPage();
-
     try {
+        const browser = await puppeteer.launch({ headless: false });
+        const page = await browser.newPage();
+
         page.on('response', async (response) => {
-            if (response.request().method() === 'POST') {
-                if (response.url() === 'https://maps.googleapis.com/$rpc/google.internal.maps.mapsjs.v1.MapsJsInternalService/GetMetadata') {
-                    const responseText = await response.text();
-                    const res = JSON.parse(responseText);
+            const url = 'https://maps.googleapis.com/$rpc/google.internal.maps.mapsjs.v1.MapsJsInternalService/GetMetadata';
+            if (response.request().method() !== 'POST' || response.url() !== url) {
+                return;
+            }
 
-                    if (res && res[1] && res[1][0] && res[1][0][3] && res[1][0][3][2]) {
-                        const locText = res[1][0][3][2];
+            const responseText = await response.text();
+            const res = JSON.parse(responseText);
+            const locText = res?.[1]?.[0]?.[3]?.[2];
+            const locCoords = res?.[1]?.[0]?.[5]?.[0]?.[1]?.[0];
+            const gameToken = page.url().split('/')[4];
 
-                        if (locText[0] && locText[0][0]) {
-                            if (locText[0] && locText[0][0] && locText[1] && locText[1][0])
-                                console.log(`${locText[0][0]} - ${locText[1][0]}`);
-                            else
-                                console.log(locText[0][0]);
-                        } else {
-                            console.log('Impossible to retrieve loc text');
-                            console.log(JSON.stringify(res));
-                        }
-                    }
-                    if (res && res[1] && res[1][0] && res[1][0][5] && res[1][0][5][0] && res[1][0][5][0][1][0]) {
-                        const locCoords = res[1][0][5][0][1][0];
-                        const lat = locCoords[2];
-                        const lng = locCoords[3];
+            if (locText) {
+                const locationName = locText[0]?.[0] || 'Unknown';
+                const locationDetails = locText[1]?.[0] || '';
+                console.log(`${locationName} - ${locationDetails}`);
+            } else {
+                console.log('Impossible to retrieve loc text');
+                console.log(JSON.stringify(res));
+            }
 
-                        console.log(lat, lng);
-                        const gameToken = page.url().split('/')[4];
+            if (locCoords) {
+                const lat = locCoords[2];
+                const lng = locCoords[3];
+                console.log(lat, lng);
 
-                        await page.evaluate(async (gameToken, lat, lng) => {
-                            console.log(gameToken);
-                            const response = await fetch(`https://www.geoguessr.com/api/v3/games/${gameToken}`, {
-                                "headers": {
-                                    "accept": "*/*",
-                                    "accept-language": "en,fr;q=0.9",
-                                    "content-type": "application/json",
-                                    "sec-ch-ua": "\"Not.A/Brand\";v=\"8\", \"Chromium\";v=\"114\", \"Google Chrome\";v=\"114\"",
-                                    "sec-ch-ua-mobile": "?0",
-                                    "sec-ch-ua-platform": "\"Windows\"",
-                                    "sec-fetch-dest": "empty",
-                                    "sec-fetch-mode": "cors",
-                                    "sec-fetch-site": "same-origin",
-                                    "x-client": "web"
-                                },
-                                "referrer": `https://www.geoguessr.com/game/${gameToken}`,
-                                "referrerPolicy": "strict-origin-when-cross-origin",
-                                "body": JSON.stringify({
-                                    token: gameToken,
-                                    lat: lat,
-                                    lng: lng,
-                                    timeout: false
-                                }),
-                                "method": "POST",
-                                "mode": "cors",
-                                "credentials": "include"
-                            });
-                            const data = await response.json();
-                            return data;
-                        }, gameToken, lat, lng);
-                    }
-                }
+                await sendAnswer(page, gameToken, lat, lng);
             }
         });
+
+        await page.goto('https://www.geoguessr.com/');
     } catch (e) {
         console.error(e);
     }
-
-    await page.goto('https://www.geoguessr.com/');
 }
 
 run();
